b9ccb6d232e75bbd2dd87d908f713a5b2253d7e3
gersondantas <gerson.accari@hotmail.com>
2024-05-08 20:20:21 -0300

    refactor: remove unnecessary code

--- src/verify_libs_folder/calc.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from "@/verify_libs_folder/math";
-
-const n1 = 10;
-const n2 = 5;
-
-const result = sum(n1, n2);
-console.log("result: ", result);
--- src/verify_libs_folder/math.ts
+++
@@ -1,6 +0,0 @@
-export const sum = (number1: number, number2: any): number | null => {
-  if (number2 < 0) {
-    return null;
-  }
-  return number1 + number2;
-};
