9ab3d37ab50d6fe35e2bdf679a218cdf957fc851
gersondantas <gerson.accari@hotmail.com>
2024-05-21 20:33:26 -0300

    test: ensure SignUpController return 400 if no other fields provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup'
 
 describe('SignUpController', () => {
-  test('Should SignUpController return 400 if no name is provider', async () => {
+  test('Should SignUpController return 400 if no name is not provider', async () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -17,7 +17,7 @@ describe('SignUpController', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 
-  test('Should SignUpController return 400 if no email is provider', async () => {
+  test('Should SignUpController return 400 if no email is not provider', async () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -31,5 +31,39 @@ describe('SignUpController', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+
+  test('Should SignUpController return 400 if no password is not provider', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@hotmail.com',
+        passwordConfirmation: 'any_password',
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: password'))
+  })
+
+  test('Should SignUpController return 400 if no passwordConfirmation is not provider', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@hotmail.com',
+        password: 'any_password',
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(
+      new Error('Missing param: passwordConfirmation'),
+    )
   })
 })
