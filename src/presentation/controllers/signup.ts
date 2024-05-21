97e7ca456049cca6948fca55ccc8a9fc1ee5bf3d
gersondantas <gerson.accari@hotmail.com>
2024-05-21 20:18:42 -0300

    feat: ensure SignUpController return 400 if no email is provide

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,10 +3,22 @@ import { HttpRequest, HttpResponse } from './protocols/http'
 
 export class SignUpController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    console.log('httpRequest', httpRequest)
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      }
+    }
+
     return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
+      statusCode: 0,
     }
   }
 }
