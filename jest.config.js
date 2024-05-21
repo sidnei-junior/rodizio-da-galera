475ccd0b4affe52f85babc37f0dc5c06f21b9db4
gersondantas <gerson.accari@hotmail.com>
2024-05-20 21:13:53 -0300

    fix: remove depreciated libs

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': 'ts-jest',
   },
-};
+}
